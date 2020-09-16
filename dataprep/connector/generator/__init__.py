9501d50c4ba810b997a1573388cca09070fb9c96
Yejia Liu <yejia@Yejias-MacBook-Pro.local>
2020-09-15 19:06:04 -0700

    feat(connector): add generator UI

--- dataprep/connector/generator/__init__.py
+++ dataprep/connector/generator/__init__.py
@@ -1,5 +1,5 @@
 """ConfigGenerator"""
-
 from .generator import ConfigGenerator
+from .ui import ConfigGeneratorUI
 
-__all__ = ["ConfigGenerator"]
+__all__ = ["ConfigGenerator", "ConfigGeneratorUI"]
